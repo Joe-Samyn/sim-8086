e8e94374004833da4649f27a95c7590330c36737
Joseph Samyn <joseph.j.samyn@gmail.com>
2026-01-17 12:11:04 -0700

    [FEATURE] Initial commit with Visual Studio CMake C++ project

---
+++ sim8086/sim8086/res/single_reg_mov.asm
@@ -0,0 +1,8 @@
+; ==========================================
+; Description: Single register move example
+; ==========================================
+
+; Tell assembler to generate 16-bit assembly code 
+bits 16
+
+mov cx, bx
---
+++ sim8086/sim8086/src/main.cpp
@@ -0,0 +1,12 @@
+﻿// sim8086.cpp : Defines the entry point for the application.
+//
+
+#include "sim8086.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
