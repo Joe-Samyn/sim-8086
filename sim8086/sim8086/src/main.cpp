a4defb001c950661504e1d99899491d16f1bfa25
Joseph Samyn <joseph.j.samyn@gmail.com>
2026-01-27 08:56:00 -0700

    [REFACTOR] Cleaned up the main file and split reading, searching, and decoding logic into functions for better readability. Setting up structure for register to memory and memory to register mov.

--- sim8086/sim8086/src/main.cpp
+++ sim8086/sim8086/src/main.cpp
@@ -1,6 +1,12 @@
 ﻿// sim8086.cpp : Defines the entry point for the application.
 //
 
+/*
+TODO: 
+	- We could just hardcode the buffer to specific size that matches the program memory size
+	of most 8086 systems/chips since we are building a simulator
+*/
+
 #include "sim8086.h"
 
 #include <fstream>
@@ -85,6 +91,11 @@ std::unordered_map<uint8_t, const char*> registerTable = {
 
 using namespace std;
 
+/**
+ * @brief Reads binary file into memory
+ * @param filePath 
+ * @param buffer 
+ */
 void readBinaryFile(char* filePath, std::vector<uint8_t> &buffer)
 {
 	std::ifstream file(filePath, std::ios::binary | std::ios::ate);
@@ -108,6 +119,97 @@ void readBinaryFile(char* filePath, std::vector<uint8_t> &buffer)
 	std::cout << "File read successfully.\n";
 }
 
+/**
+ * @brief Find the instruction whose opcode matches the byte 
+ * @param byte 
+ * @return 
+ */
+int findInstruction(uint8_t byte)
+{
+	for (int i = 0; i < instructionTable.size(); i++)
+	{
+		// Get opcode from byte
+		InstructionEntry inst = instructionTable.at(i);
+		uint8_t b = byte & inst.opcodeMask;
+
+		// If byte matches opcode, break out and return index of instruction
+		if (b == inst.opcode)
+		{
+			return i;
+		}
+	}
+
+	// Byte doesn't match any instructions
+	return -1;
+}
+
+/**
+ * @brief Decode instruction using entry in 8086 table
+ * @param instruction 
+ * @param entry 
+ */
+void decodeInstruction(Instruction &instruction, InstructionEntry &entry, std::vector<uint8_t> program, int &programIndex)
+{
+	// Begin filling in instruction details
+	instruction.opcode = entry.opcode;
+	instruction.mnemonic = entry.mnemonic;
+
+	// TODO: Do we need to adjust length or do anything new if dMask or wMask are present? 
+	// Check if instruction contains Direction Mask
+	if (entry.dMask != 0)
+	{
+		instruction.direction = program.at(programIndex) & entry.dMask;
+	}
+
+	// Check if instruction contains Direction Mask
+	if (entry.wMask != 0)
+	{
+		instruction.width = program.at(programIndex) & entry.wMask;
+	}
+
+	if (entry.modMask != 0)
+	{
+		programIndex++;
+		uint8_t mod = program.at(programIndex) & entry.modMask;
+
+		switch (mod)
+		{
+			// Memory mode, no displacement
+		case 0b00000000:
+		{
+			// TODO
+		} break;
+
+		// Memory mode, 8-bit displacement
+		case 0b10000000:
+		{
+			// TODO
+		} break;
+
+		// Memory mode, 16-bit displacement
+		case 0b01000000:
+		{
+			// TODO
+		} break;
+
+		// Register mode, no displacement
+		case 0b11000000:
+		{
+			// Both operands are in registers, so decode the registers involved in the operation
+			uint8_t registerA = (program.at(programIndex) & entry.regMask) >> 3;
+			uint8_t registerB = program.at(programIndex) & entry.rmMask;
+
+
+			instruction.registerA = instruction.direction ? registerA : registerB;
+			instruction.registerB = instruction.direction ? registerB : registerA;
+			instruction.registerAName = instruction.width ? registerWideTable.at(instruction.registerA) : registerTable.at(instruction.registerA);
+			instruction.registerBName = instruction.width ? registerWideTable.at(instruction.registerB) : registerTable.at(instruction.registerB);
+
+		} break;
+		}
+	}
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -125,107 +227,39 @@ int main(int argc, char* argv[])
 	// Read binary file into a vector if bytes
 	readBinaryFile(argv[1], buffer);
 
+	// Ensure file was read into buffer
 	if (buffer.size() < 1)
 	{
 		return 1;
 	}
 
-	// Parse bytes
+	// Begin decoding bytes one at a time
 	std::cout << "\n\nbits 16\n\n\n";
-	int currentByteIndex = 0;
-	while (currentByteIndex < buffer.size())
+	int programIndex = 0;
+	while (programIndex < buffer.size())
 	{
-		int instIndex = -1;
-		uint8_t currentByte = buffer.at(currentByteIndex);
-		for (int i = 0; i < instructionTable.size(); i++)
-		{
-			// Get opcode from byte
-			InstructionEntry inst = instructionTable.at(i);
-			uint8_t b = buffer.at(currentByteIndex) & inst.opcodeMask;
-
-			// Check if opcode is matches current instruction entry
-			if (b == inst.opcode)
-			{
-				instIndex = i;
-				break;
-			}
-		}
+		
+		uint8_t currentByte = buffer.at(programIndex);
+		int instructionIndex = findInstruction(currentByte);
 
 		// Check if an instruction was found
-		if (instIndex == -1)
+		if (instructionIndex == -1)
 		{
 			std::cout << std::format("The byte did not map to a valid 8086 instruction::{}\n", std::bitset<8>(currentByte).to_string());
 			return 1;
 		}
 
-		InstructionEntry instructionEntry = instructionTable.at(instIndex);
+		InstructionEntry instructionEntry = instructionTable.at(instructionIndex);
 		//std::cout << std::format("Instruction: {}, {}\n", instructionEntry.description, std::bitset<8>(instructionEntry.opcode).to_string());
 
 		// Begin filling in instruction details
 		Instruction instruction = { 0 };
-		instruction.opcode = instructionEntry.opcode;
-		instruction.mnemonic = instructionEntry.mnemonic;
-		
-		// TODO: Do we need to adjust length or do anything new if dMask or wMask are present? 
-		// Check if instruction contains Direction Mask
-		if (instructionEntry.dMask != 0)
-		{
-			instruction.direction = currentByte & instructionEntry.dMask;
-		}
-
-		// Check if instruction contains Direction Mask
-		if (instructionEntry.wMask != 0)
-		{
-			instruction.width = currentByte & instructionEntry.wMask;
-		}
-
-		if (instructionEntry.modMask != 0)
-		{
-			currentByteIndex++;
-			currentByte = buffer.at(currentByteIndex);
-			uint8_t mod = currentByte & instructionEntry.modMask;
-
-			switch (mod)
-			{
-				// Memory mode, no displacement
-				case 0b00000000:
-				{
-					// TODO
-				} break;
-
-				// Memory mode, 8-bit displacement
-				case 0b10000000:
-				{
-					// TODO
-				} break;
-
-				// Memory mode, 16-bit displacement
-				case 0b01000000:
-				{
-					// TODO
-				} break;
-
-				// Register mode, no displacement
-				case 0b11000000:
-				{
-					// Both operands are in registers, so decode the registers involved in the operation
-					uint8_t registerA = (currentByte & instructionEntry.regMask) >> 3;
-					uint8_t registerB = currentByte & instructionEntry.rmMask;
-
-					
-					instruction.registerA = instruction.direction ? registerA : registerB;
-					instruction.registerB = instruction.direction? registerB: registerA;
-					instruction.registerAName = instruction.width ? registerWideTable.at(instruction.registerA) : registerTable.at(instruction.registerA);
-					instruction.registerBName = instruction.width ? registerWideTable.at(instruction.registerB) : registerTable.at(instruction.registerB);
-
-				} break;
-			}
-
-			currentByteIndex++;
-		}
+		decodeInstruction(instruction, instructionEntry, buffer, programIndex);
 
 		// Print instruction 
 		std::cout << std::format("{} {}, {}\n", instruction.mnemonic, instruction.registerAName, instruction.registerBName);
+
+		programIndex++;
 	}
 
 
