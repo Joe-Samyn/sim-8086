c64b47fd84a42071850a5c3d36a4fe3f5c36c167
Joe Samyn <joseph.j.samyn@gmail.com>
2026-08-30 10:46:56 -0500

    Add additional tests for ExecuteAdd with various register operations

--- sim8086/sim8086/tests/TestExecuteADD.cpp
+++ sim8086/sim8086/tests/TestExecuteADD.cpp
@@ -157,10 +157,240 @@ TEST(ExecuteAdd_FullRegisterToFullRegisterSignFlagSet, {
     ASSERT_EQUAL(Sign, sf);
 })
 
+TEST(ExecuteAdd_LoRegisterToLoRegisterAddNoFlagsSet, {
+    // ARRANGE
+    CPU cpu = {};
+    cpu.registers[Register_b] = 0xFF01;
+    cpu.registers[Register_c] = 0xFF02;
+
+    uint8_t size = BYTE;
+
+    Operand src = {};
+    src.type = OpType_register;
+    src.reg.index = Register_b;
+    src.reg.offset = LO_BITS;
+
+    Operand dest = {};
+    dest.type = OpType_register;
+    dest.reg.index = Register_c;
+    dest.reg.offset = LO_BITS;
+
+    int exp = 0x03;
+
+    // ACT
+    ExecuteAdd(cpu, src, dest, size);
+    uint8_t result = ReadLoByte(cpu.registers[Register_c]);
+
+    // ASSERT
+    ASSERT_EQUAL(exp, result);
+    ASSERT_EQUAL(0, cpu.flags);
+})
+
+TEST(ExecuteAdd_LoByteToLoByteCarryFlagSet, {
+    // ARRANGE
+    CPU cpu = {};
+    cpu.registers[Register_b] = 0x00FF;
+    cpu.registers[Register_c] = 0xAB02;
+
+    uint8_t size = BYTE;
+
+    Operand src = {};
+    src.type = OpType_register;
+    src.reg.index = Register_b;
+    src.reg.offset = LO_BITS;
+
+    Operand dest = {};
+    dest.type = OpType_register;
+    dest.reg.index = Register_c;
+    dest.reg.offset = LO_BITS;
+
+    int exp = 0xAB01;
+    int expLo = 0x01;
+
+    // ACT
+    ExecuteAdd(cpu, src, dest, size);
+    uint16_t result = cpu.registers[Register_c];
+    uint8_t resultLo = ReadLoByte(cpu.registers[Register_c]);
+    uint16_t cf = cpu.flags & Carry;
+
+    // ASSERT
+    ASSERT_EQUAL(exp, result);
+    ASSERT_EQUAL(expLo, resultLo);
+    ASSERT_EQUAL(Carry, cf);
+})
+
+TEST(ExecuteAdd_LoByteToLoByteSignFlagSet, {
+    // ARRANGE
+    CPU cpu = {};
+    cpu.registers[Register_b] = 0xFFF1;
+    cpu.registers[Register_c] = 0xAB01;
+
+    uint8_t size = BYTE;
+
+    Operand src = {};
+    src.type = OpType_register;
+    src.reg.index = Register_b;
+    src.reg.offset = LO_BITS;
+
+    Operand dest = {};
+    dest.type = OpType_register;
+    dest.reg.index = Register_c;
+    dest.reg.offset = LO_BITS;
+
+    int exp = 0xABF2;
+    int expLo = 0xF2;
+
+    // ACT
+    ExecuteAdd(cpu, src, dest, size);
+    uint16_t result = cpu.registers[Register_c];
+    uint8_t resultLo = ReadLoByte(cpu.registers[Register_c]);
+    uint16_t sf = cpu.flags & Sign;
+
+    // ASSERT
+    ASSERT_EQUAL(exp, result);
+    ASSERT_EQUAL(expLo, resultLo);
+    ASSERT_EQUAL(Sign, sf);
+})
+
+TEST(ExecuteAdd_LoByteToLoByteOverflowFlagSet, {
+    // ARRANGE
+    CPU cpu = {};
+    cpu.registers[Register_b] = 0x1155;
+    cpu.registers[Register_c] = 0x1156;
+
+    uint8_t size = BYTE;
+
+    Operand src = {};
+    src.type = OpType_register;
+    src.reg.index = Register_b;
+    src.reg.offset = LO_BITS;
+
+    Operand dest = {};
+    dest.type = OpType_register;
+    dest.reg.index = Register_c;
+    dest.reg.offset = LO_BITS;
+
+    int exp = 0x11AB;
+    int expLo = 0xAB;
+
+    // ACT
+    ExecuteAdd(cpu, src, dest, size);
+    uint16_t result = cpu.registers[Register_c];
+    uint8_t resultLo = ReadLoByte(cpu.registers[Register_c]);
+    uint16_t of = cpu.flags & Overflow;
+
+    // ASSERT
+    ASSERT_EQUAL(exp, result);
+    ASSERT_EQUAL(expLo, resultLo);
+    ASSERT_EQUAL(Overflow, of);
+})
+
+TEST(ExecuteAdd_LoByteToLoByteZeroFlagSet, {
+    // ARRANGE
+    CPU cpu = {};
+    cpu.registers[Register_b] = 0x11FF;
+    cpu.registers[Register_c] = 0x1101;
+
+    uint8_t size = BYTE;
+
+    Operand src = {};
+    src.type = OpType_register;
+    src.reg.index = Register_b;
+    src.reg.offset = LO_BITS;
+
+    Operand dest = {};
+    dest.type = OpType_register;
+    dest.reg.index = Register_c;
+    dest.reg.offset = LO_BITS;
+
+    int exp = 0x1100;
+    int expLo = 0x00;
+
+    // ACT
+    ExecuteAdd(cpu, src, dest, size);
+    uint16_t result = cpu.registers[Register_c];
+    uint8_t resultLo = ReadLoByte(cpu.registers[Register_c]);
+    uint16_t zf = cpu.flags & Zero;
+
+    // ASSERT
+    ASSERT_EQUAL(exp, result);
+    ASSERT_EQUAL(expLo, resultLo);
+    ASSERT_EQUAL(Zero, zf);
+})
+
+TEST(ExecuteAdd_LoByteToHiByte, {
+    // ARRANGE
+    CPU cpu = {};
+    cpu.registers[Register_b] = 0x01AB;
+    cpu.registers[Register_c] = 0xCD20;
+
+    uint8_t size = BYTE;
+
+    Operand src = {};
+    src.type = OpType_register;
+    src.reg.index = Register_c;
+    src.reg.offset = LO_BITS;
+
+    Operand dest = {};
+    dest.type = OpType_register;
+    dest.reg.index = Register_b;
+    dest.reg.offset = HI_BITS;
+
+    int exp = 0x21AB;
+    int expHi = 0x21;
+
+    // ACT
+    ExecuteAdd(cpu, src, dest, size);
+    uint16_t result = cpu.registers[Register_b];
+    uint8_t resultHi = ReadHiByte(cpu.registers[Register_b]);
+
+    // ASSERT
+    ASSERT_EQUAL(exp, result);
+    ASSERT_EQUAL(expHi, resultHi);
+})
+
+TEST(ExecuteAdd_HiByteToLoByte, {
+    // ARRANGE
+    CPU cpu = {};
+    cpu.registers[Register_b] = 0x01AB;
+    cpu.registers[Register_c] = 0xCD22;
+
+    uint8_t size = BYTE;
+
+    Operand src = {};
+    src.type = OpType_register;
+    src.reg.index = Register_b;
+    src.reg.offset = HI_BITS;
+
+    Operand dest = {};
+    dest.type = OpType_register;
+    dest.reg.index = Register_c;
+    dest.reg.offset = LO_BITS;
+
+    int exp = 0xCD23;
+    int expLo = 0x23;
+
+    // ACT
+    ExecuteAdd(cpu, src, dest, size);
+    uint16_t result = cpu.registers[Register_c];
+    uint8_t resultLo = ReadLoByte(cpu.registers[Register_c]);
+
+    // ASSERT
+    ASSERT_EQUAL(exp, result);
+    ASSERT_EQUAL(expLo, resultLo);
+})
+
 TESTS(ExecuteAddTests) = {
     ExecuteAdd_FullRegisterToFullRegisterNoFlagsSet,
     ExecuteAdd_FullRegisterToFullRegisterCarryFlagSet,
     ExecuteAdd_FullRegisterToFullRegisterOverflowFlagSet,
     ExecuteAdd_FullRegisterToFullRegisterZeroFlagSet,
-    ExecuteAdd_FullRegisterToFullRegisterSignFlagSet
+    ExecuteAdd_FullRegisterToFullRegisterSignFlagSet,
+    ExecuteAdd_LoRegisterToLoRegisterAddNoFlagsSet,
+    ExecuteAdd_LoByteToLoByteCarryFlagSet,
+    ExecuteAdd_LoByteToLoByteSignFlagSet,
+    ExecuteAdd_LoByteToLoByteOverflowFlagSet,
+    ExecuteAdd_LoByteToLoByteZeroFlagSet,
+    ExecuteAdd_LoByteToHiByte,
+    ExecuteAdd_HiByteToLoByte
 };
