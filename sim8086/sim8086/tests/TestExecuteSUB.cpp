fd18c018979e22b48530cb828ba1aa461ee5ac52
Joe Samyn <joseph.j.samyn@gmail.com>
2026-09-02 08:30:34 -0500

    Update ExecuteSub test to use BYTE offset for destination register in ImmediateFromLoByte test

--- sim8086/sim8086/tests/TestExecuteSUB.cpp
+++ sim8086/sim8086/tests/TestExecuteSUB.cpp
@@ -453,7 +453,7 @@ TEST(ExecuteSub_ImmediateFromLoByteNoFlags, {
     Operand dest = {};
     dest.type = OpType_register;
     dest.reg.index = Register_b;
-    dest.reg.offset = FULL_BITS;
+    dest.reg.offset = BYTE;
 
     Operand src = {};
     src.type = OpType_immediate;
